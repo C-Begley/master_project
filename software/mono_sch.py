7ec66f1ae0023ba9a708c21347a342ca4ff34108
C-Begley <35376967+C-Begley@users.noreply.github.com>
2020-11-10 16:25:26 +0000

    First iteration of tasks scheduled

--- software/mono_sch.py
+++ software/mono_sch.py
@@ -1,6 +1,8 @@
 import json
 import time
 
+PERIOD = 1  #Update with scheduling frequency of pycubed
+
 def lcm_2(x, y):
    if x > y:
        greater = x
@@ -36,24 +38,42 @@ def possible_schedule(tasks):
     return True
 
 def schedule_order(tasks):
-    periods = [tasks[task]["period"] for task in tasks]
+    periods = sorted([tasks[task]["period"] for task in tasks])
     sch_period = lcm_arr(periods)
+    print(tasks)
     mock_time = 0
     task_pattern = []
-    while mock_time < sch_period:
-        pass
+    tasks_list = sorted_x = sorted(tasks.items(), key=lambda kv: kv[1]["period"])
+    tasks_list = [x[0] for x in tasks_list]
+    initial = []
+    next_deadline = {}
+    schedlued = {}
+
+    #Initial Scheduling
+    i = 0
+    while len(initial) != len(tasks):
+            next_t = tasks_list[i]
+            for task in initial:
+                if (mock_time+tasks[next_t]["time"]) > next_deadline[task] :
+                    task_pattern.append(task)
+                    mock_time += tasks[task]["time"]
+                    next_deadline[task] = tasks[task]["period"] + mock_time
+            task_pattern.append(next_t)
+            mock_time+=tasks[next_t]["time"]
+            initial.append(next_t)
+            next_deadline[next_t] = tasks[next_t]["period"] + mock_time
+            i += 1
+    return task_pattern
 
 
 
 
 sample = json.loads('{"coms":{"time":10, "period":30}')
-samples = json.loads('{"coms":{"time":10, "period":30}, "payload_1":{"time":10, "period":50}, "payload_2":{"time":10, "period":100}}')
+samples = json.loads('{"P2":{"time":2, "period":8}, "P1":{"time":1, "period":5}, "P3":{"time":2, "period":10}}')
+
 bad_samples = json.loads('{"coms":{"time":10, "period":30}, "payload":{"time":15, "period":20} }')
+
 '''
-x = json.loads('[{"coms":{"time":10, "period":30}]')
-print(x)
-
 initial = time.monotonic()
-
 while True:
 '''
